38f3735e59ccfa106a21a6203d93ee93fff1331a
Manuelb1990 <manuell@bluewin.ch>
2022-10-24 22:07:17 +0200

    add different routes and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { BoardComponent } from './board/board.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { LoginComponent } from './login/login.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: 'board', component: BoardComponent },
+  { path: 'addtask', component: AddTaskComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '', component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SummaryComponent } from './summary/summary.component';
+import { BoardComponent } from './board/board.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    SummaryComponent
+    SummaryComponent,
+    BoardComponent,
+    AddTaskComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
