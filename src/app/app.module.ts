693b29ea017292bc8a0455e43c4579f1fd135f97
Manuelb1990 <manuell@bluewin.ch>
2022-10-24 19:55:46 +0200

    create menu and header, start with summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
