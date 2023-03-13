15ec1a57c35c74adacf82d6ec2fae2a808d18812
Manuelb1990 <manuell@bluewin.ch>
2023-03-13 19:40:35 +0100

    Revert "Revert "start with Board Arry and Drop Down""
    This reverts commit 8dfddbe34ca76c6ed42e9fcfe2f4f15028366a72.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-board',
@@ -7,25 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit {
-  // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  public todos = [{
+    'id': 0,
+    'title': 'Website redesign',
+    'category': 'toDo',
+    'tag': 'Design',
+    'text': 'Modify the contents of the main website...',
+    'stateNow': 1,
+    'stateTotal': 4,
+    'contacts': {
+      'names': ['MB', 'MV', 'EF'],
+      'colors': ['#fd2829', '#b88770', '#007bff']
+    },
+    'priority': 'low',
 
-  // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-  // drop(event: CdkDragDrop<string[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex,
-  //     );
-  //   }
-  // }
 
-  constructor() { }
+  },
+  {
+    'id': 1,
+    'title': 'Call potential clients',
+    'category': 'inProgress',
+    'tag': 'Sales',
+    'text': 'Make the product presen-tation to prospective buyers',
+    'stateNow': 0,
+    'stateTotal': 3,
+    'contacts': {
+      'names': ['MB', 'MV', 'EF', 'SO'],
+      'colors': ['#fd2829', '#b88770', '#007bff', '#2dfcd7']
+    },
+    'priority': 'urgent',
+
+  }
+  ];
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    console.log(this.todos)
   }
 
 }
